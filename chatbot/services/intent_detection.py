089ab87a9acd848c9d304c8107cdf5fa667bff7f
nagarajs14 <nagarajs.aiminds@gmail.com>
2026-03-11 15:51:14 +0530

    feat: add chatbot module with intent detection

--- chatbot/services/intent_detection.py
+++ chatbot/services/intent_detection.py
@@ -1,6 +1,66 @@
 def detect_intent(message):
 
     message = message.lower()
+    
+    # track_keywords = [
+    #     "track", 
+    #     "where is my package", 
+    #     "shipment status", 
+    #     "track order"
+    #     ] 
+    
+    # delivery_keywords = [
+    #     "delivery time", 
+    #     "when will my package arrive", 
+    #     "eta"
+    #     ]
+    
+    # driver_keywords = [
+    #     "call driver", 
+    #     "driver phone", 
+    #     "contact driver"
+    #     ]
+    
+    # damaged_keywords = [
+    #     "package damaged", 
+    #     "parcel broken"
+    #     ]
+    
+    # delay_keywords = [
+    #     "driver not arrived", 
+    #     "delivery delay"
+    #     ]
+    
+    # address_keywords = [
+    #     "wrong address", 
+    #     "change address"
+    #     ]
+    
+    # for word in track_keywords:
+    #     if word in message:
+    #         return "track_shipment"
+        
+    # for word in delivery_keywords:
+    #     if word in message:
+    #         return "delivery_time"
+        
+    # for word in driver_keywords:
+    #     if word in message:
+    #         return "driver_contact"
+        
+    # for word in damaged_keywords:
+    #     if word in message:
+    #         return "package_damaged"
+        
+    # for word in delay_keywords:  
+    #     if word in message:
+    #         return "driver_delay"  
+
+    # for word in address_keywords:  
+    #     if word in message:
+    #         return "wrong_address"  
+
+    # return "unknown"
 
     if "track" in message or "where is my package" in message:
         return "track_shipment"
